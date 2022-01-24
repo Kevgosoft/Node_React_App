2b6522b123d99c06f0371eaa45dfc2f1bdc85ae6
Kevgosoft <93751777+Kevgosoft@users.noreply.github.com>
2022-01-24 13:11:32 -0600

    Add files via upload

---
+++ backend/routes/handler.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/tweets', (req, res) => {
+    const str = [{
+       "name": "Codr Kai",
+       "msg": "This is my first tweet!",
+       "username": "codrkai"
+    }];
+    res.end(JSON.stringify(str));
+});
+
+router.post('/addTweet', (req, res) => {
+    res.end('NA');
+});
+
+module.exports = router;
